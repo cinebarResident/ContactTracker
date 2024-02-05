0a312e428e128bca429edfedd7daf98ced5be30d
Brittany Hancock <brittany.hancock@student.centralia.edu>
2024-02-05 14:42:35 -0800

    Add project files.

---
+++ ContactTracker.Server/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactTracker.Server.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; } // Primary key
+
+        // Other properties
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        // Navigation property for relational data - one customer to many contact attempts
+        public ICollection<ContactAttempt> ContactAttempts { get; set; }
+    }
+}
